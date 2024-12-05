78d6e8a82e2d3436b30296dde7042a5f8943272c
Bereket Getachew <bakiget78@gmail.com>
2024-12-05 20:29:43 +0300

    Update types.ts

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -28,22 +28,22 @@ export interface OptionsProps {
 }
 
 export interface ConstantType {
-  pi: number; // Mathematical constant π
-  gravity: number; // Earth's gravity in m/s^2
-  speedOfLight: number; // Speed of light in meters per second
-  euler: number; // Euler's number
-  plancksConstant: number; // Planck's constant in J·s
-  gravitationalConstant: number; // Gravitational constant in m^3·kg^−1·s^−2
-  avogadro: number; // Avogadro's number in particles/mol
-  boltzmann: number; // Boltzmann constant in J/K
+  pi: number; 
+  gravity: number;
+  speedOfLight: number; 
+  euler: number; 
+  plancksConstant: number; 
+  gravitationalConstant: number; 
+  avogadro: number;
+  boltzmann: number;
 }
 
 export interface Units {
-  kg: number; // Base unit in kilograms
-  g: number; // Grams
-  lb: number; // Pounds
-  oz: number; // Ounces
-  mg: number; // Milligrams
-  [key: string]: number; // Index signature to allow any string key
+  kg: number;
+  g: number;
+  lb: number;
+  oz: number; 
+  mg: number;
+  [key: string]: number;
 }
 
